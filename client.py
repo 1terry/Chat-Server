c9cc087208f0665ddcc3c850488e7b26c79b3497
1terry <t.juottawa@gmail.com>
2021-10-07 13:18:44 -0400

    initialized files

---
+++ client.py
@@ -0,0 +1,8 @@
+import argparse
+import selectors
+import socket
+import os
+import sys
+from urllib.parse import urlparse
+
+username = ""
---
+++ server.py
@@ -0,0 +1,13 @@
+import argparse
+import selectors
+import socket
+import os
+import sys
+from urllib.parse import urlparse
+
+serverAdress = ""
+
+# useful documentation
+
+# https://docs.python.org/3/library/argparse.html
+# https://docs.python.org/3/library/urllib.parse.html#module-urllib.parse
