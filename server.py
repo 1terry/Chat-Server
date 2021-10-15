d0e923714a716c7a5effbe2bf5f73f0e88c8206f
Terry <t.juottawa@gmail.com>
2021-10-15 02:19:36 -0400

    added starting code

--- server.py
+++ server.py
@@ -3,9 +3,21 @@ import selectors
 import socket
 import os
 import sys
+import signal
 from urllib.parse import urlparse
 
-serverAdress = ""
+from socket import *
+serverPort = 12000
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
+print('The server is ready to receive')
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    capsSentence = sentence.upper()
+    connectionSocket.send(capsSentence.encode())
+    connectionSocket.close()
 
 # useful documentation
 
