72faee4bfcd0b982fbc6c869df3ec413f2af1d2a
Terry <t.juottawa@gmail.com>
2021-10-21 21:18:20 -0400

    Commented and finalized code

--- client2/client.py
+++ client2/client.py
@@ -1,3 +1,12 @@
+"""
+File for chat client
+Created for CS 3357 By Terrence Ju
+Oct 21, 2021
+Client will take in parameters as arguments in the terminal and connect to the server
+Client will be able to connect to other clients and send and recieve messages from them
+"""
+
+#Libraries and packages used
 import selectors
 import socket
 import sys
@@ -5,58 +14,63 @@ import select
 import signal
 from urllib.parse import urlparse
 
+#Initializes variables
 mysel = selectors.DefaultSelector()
 keep_running = True
-# argument_count = (len(sys.argv))
 inputArgs = sys.argv
 
-#Isolates user name and host address from input
-inputUser = inputArgs[1]
-formattedAddress = inputArgs[2]
-
-#Gets address from input and sets as 
-parsedAddress = urlparse(formattedAddress) 
-hostAddress = parsedAddress[1]
-hostName, hostPort = hostAddress.split(":", 1)
-hostPort = int(hostPort) 
-
-#Registers socket with server using the parsed parameters
-server_address = (hostName, hostPort)
-print('connecting to {} port {}'.format(*server_address))
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+#Tries connecting to server using formatted input from user
 try:
+    #Exits on wrong format
+    if (len(inputArgs) > 3):
+        print("Too many arguments")
+        sys.exit()
+
+    #Parses user name and server address from parameters and stores as variables
+    inputUser = inputArgs[1]
+    formattedAddress = inputArgs[2]
+    parsedAddress = urlparse(formattedAddress) 
+    hostAddress = parsedAddress[1]
+    hostName, hostPort = hostAddress.split(":", 1)
+    hostPort = int(hostPort) 
+
+    #Registers socket with server using the parsed parameters, tries connecting to server and sets blocking to flase
+    server_address = (hostName, hostPort)
+    print('connecting to {} port {}'.format(*server_address))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(server_address)
     sock.setblocking(False)
     print("Connection succesful, sending intro message...")
+
+#Client closes on bad input
 except:
-    print("Server not found")
+    print("Server not found, check your commands and run again")
     doNothing = True
     sys.exit()
 
-#Registers socket
-mysel.register(
-    sock, selectors.EVENT_WRITE
-)
-
-#Registers user
+#Registers socket and sends registration message
+mysel.register(sock, selectors.EVENT_WRITE)
 sock.send(("REGISTER " + inputUser + " CHAT/1.0").encode())
-succesful = False
 
+#Infinite loop to recieve input from server
 while keep_running:
     try:
+        #Loops and checks for server input
         for key, mask in mysel.select(timeout=1):
             connection = key.fileobj
 
+            #Checks user input in terminal and sends to 
             input = select.select([sys.stdin], [], [], 1)[0]
             if input:
                 value = sys.stdin.readline().rstrip()
                 next_msg = (inputUser + ": " + value).encode()
                 sock.sendall(next_msg)    
 
+            #Recieves and formats data from server
             data = repr(connection.recv(1024))
             data = data[2:len(data)-1]
 
+            #Responds to 400 message by shutting everything down and printing error
             if data == "400 Invalid registration":
                 print("Error, invalid registration")
                 mysel.unregister(connection)
@@ -64,10 +78,9 @@ while keep_running:
                 mysel.close() 
                 sys.exit()
 
-            #Checks for error codes and closes connection if error detected
+            #Responds to 401 message by shutting everything down and printing error
             if data == "401 Client already registered":
                 print("Error, client already registered")
-                # next_msg = (inputUser + ": " + value).encode()
                 mysel.unregister(connection)
                 connection.close()
                 mysel.close() 
@@ -81,37 +94,27 @@ while keep_running:
                 mysel.close() 
                 sys.exit()
 
-            #try acceppting connection
-
+            #Prints message if connection is accepted succesfully
             if data == "200 Registration successful":
                 print("Server running, start messaging!")
-                succesful = True
-                # input = select.select([sys.stdin], [], [], 1)[0]
-                # outgoing = input
-                # if input:
-                #     value = sys.stdin.readline().rstrip()
-                #     next_msg = (inputUser + ": " + value).encode()
-                #     print(next_msg)
-                #     sock.sendall(next_msg)    
-                  
-            #Displays username and message recieved message only if sender is not the current user
 
-            if succesful:
-                try:
-                    username, recievedMessage = data.split(":", 1)
-                    if (username != inputUser):
-                        print("    @" + username + ": " + recievedMessage)
-                except:
-                    doNothing = True
+            #Filters server messages so that only messages from other users are displayed
+            try:
+                username, recievedMessage = data.split(":", 1)
+                if (username != inputUser):
+                    print("    @" + username + ": " + recievedMessage)
+
+            #Catches a differently formatted message and ignores
+            except:
+                doNothing = True
 
 
     #Catches socket error of recieving nothing, this just means no other clients have connected yet
     except (socket.error): 
-        # print("Exception fuck")
         doNothing = True
         pass
             
-    #Catches control-c, prints shutdown message and disconnects user from server
+    #Catches control-c, prints shutdown message and disconnects and unregisters user
     except KeyboardInterrupt:
         print ("\n Interrupt Recieved, disconnecting user")
         sock.send("DISCONNECT username CHAT/1.0".encode())
@@ -121,8 +124,7 @@ while keep_running:
         sys.exit()
         pass 
 
-    #Server is likely closed or another error has been encountered so we exit the program
-    except Exception as e:
-        print(e)
+    #Server is likely closed, so we exit the program
+    except Exception:
         sys.exit() 
 
